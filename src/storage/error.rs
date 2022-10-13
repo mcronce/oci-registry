5b9624e832a6de933ea73450d900d7094b560af6
Mike Cronce <mike@quadra-tec.net>
2022-10-13 16:52:46 -0400

    src/storage/error.rs:  Use Debug instead of Display for RusotoError messages

--- src/storage/error.rs
+++ src/storage/error.rs
@@ -4,9 +4,9 @@ use rusoto_core::RusotoError;
 pub enum Error {
 	#[error("I/O error: {0}")]
 	Io(#[from] std::io::Error),
-	#[error("Failed to get object from S3: {0}")]
+	#[error("Failed to get object from S3: {0:?}")]
 	RusotoGet(#[from] RusotoError<rusoto_s3::GetObjectError>),
-	#[error("Failed to put object into S3: {0}")]
+	#[error("Failed to put object into S3: {0:?}")]
 	RusotoPut(#[from] RusotoError<rusoto_s3::PutObjectError>),
 	#[error("Failed to parse datetime: {0}")]
 	ParseTime(#[from] time::error::Parse),

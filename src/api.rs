ee7e5a5b5a26f2f7e48ee6d98e42a7db79a5cba1
Mike Cronce <mike@quadra-tec.net>
2022-10-15 15:58:02 -0400

    src/api.rs:  Moved storage around a bit; blobs are now "global" in /blobs, while manifests remain namespaced

--- src/api.rs
+++ src/api.rs
@@ -45,21 +45,24 @@ pub struct ManifestRequest {
 }
 
 impl ManifestRequest {
-	fn path(&self) -> String {
+	fn http_path(&self) -> String {
 		format!("/{}/manifests/{}", self.image, self.reference)
+	}
+
+	fn storage_path(&self) -> String {
+		format!("manifests/{}/{}", self.image, self.reference)
 	}
 }
 
 async fn get_manifest(req: &ManifestRequest, max_age: Duration, repo: &Repository, upstream: web::Data<Client>) -> Result<Manifest, Error> {
-	let path = req.path();
-	let path = path.strip_prefix("/").unwrap();
-	match repo.clone().read(&path, max_age).await {
+	let storage_path = req.storage_path();
+	match repo.clone().read(&storage_path, max_age).await {
 		Ok(stream) => {
 			let body = stream.try_collect::<web::BytesMut>().await?;
 			let manifest = serde_json::from_slice(body.as_ref())?;
 			return Ok(manifest);
 		},
-		Err(e) => warn!("{} not found in repository ({}); pulling from upstream", path, e)
+		Err(e) => warn!("{} not found at {} in repository ({}); pulling from upstream", req.http_path(), storage_path, e)
 	}
 
 	let mut upstream = (*upstream.into_inner()).clone();
@@ -69,7 +72,7 @@ async fn get_manifest(req: &ManifestRequest, max_age: Duration, repo: &Repositor
 
 	let body = serde_json::to_vec(&manifest).unwrap();
 	let len = body.len().try_into().unwrap_or(i64::MAX);
-	if let Err(e) = repo.write(&path, stream::iter(iter::once(Result::<_, std::io::Error>::Ok(body.into()))), len).await {
+	if let Err(e) = repo.write(&storage_path, stream::iter(iter::once(Result::<_, std::io::Error>::Ok(body.into()))), len).await {
 		error!("{}", e);
 	}
 	Ok(manifest)
@@ -93,8 +96,12 @@ pub struct BlobRequest {
 }
 
 impl BlobRequest {
-	fn path(&self) -> ArcStr {
+	fn http_path(&self) -> ArcStr {
 		format!("/{}/blobs/{}", self.image, self.digest).into()
+	}
+
+	fn storage_path(&self) -> ArcStr {
+		format!("blobs/{}", self.digest).into()
 	}
 }
 
@@ -103,9 +110,9 @@ pub async fn blob(path: web::Path<BlobRequest>, invalidation: web::Data<Invalida
 		return Err(Error::InvalidDigest);
 	}
 
-	let req_path = path.path();
-	let storage_path = req_path.strip_prefix("/").unwrap();
-	match (*repo.clone().into_inner()).clone().read(storage_path, invalidation.blob).await {
+	let req_path = path.http_path();
+	let storage_path = path.storage_path();
+	match (*repo.clone().into_inner()).clone().read(storage_path.as_ref(), invalidation.blob).await {
 		Ok(stream) => return Ok(HttpResponse::Ok().streaming(stream)),
 		Err(e) => warn!("{} not found in repository ({}); pulling from upstream", storage_path, e)
 	};
@@ -117,7 +124,6 @@ pub async fn blob(path: web::Path<BlobRequest>, invalidation: web::Data<Invalida
 	let len = response.size().unwrap_or_default();
 	let (tx, rx) = async_broadcast::broadcast(16);
 	{
-		let req_path = req_path.clone();
 		let mut stream = response.stream();
 		rt::spawn(async move {
 			while let Some(chunk) = stream.next().await {
@@ -138,7 +144,7 @@ pub async fn blob(path: web::Path<BlobRequest>, invalidation: web::Data<Invalida
 
 	let rx2 = rx.clone();
 	rt::spawn(async move {
-		if let Err(e) = repo.write(req_path.strip_prefix("/").unwrap(), rx2, len.try_into().unwrap_or(i64::MAX)).await {
+		if let Err(e) = repo.write(storage_path.as_ref(), rx2, len.try_into().unwrap_or(i64::MAX)).await {
 			error!("{}", e);
 		}
 	});

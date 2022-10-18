7bcd1e231a7b05bc8054a7fa759c63979d2197d3
Mike Cronce <mike@quadra-tec.net>
2022-10-18 18:19:27 -0400

    src/api/error.rs:  Add implementations of actix_web::ResponseError::status_code() and actix_web::ResponseError::error_response() for Error; the former so that we correctly 404 on InvalidDigest instead of 500, and the latter so that we log errors when they happen

--- src/api/error.rs
+++ src/api/error.rs
@@ -1,3 +1,9 @@
+use actix_web::body::BoxBody;
+use actix_web::http::StatusCode;
+use actix_web::HttpResponse;
+use actix_web::HttpResponseBuilder;
+use tracing::error;
+
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
 	#[error("Error with storage subsystem: {0}")]
@@ -13,5 +19,21 @@ pub enum Error {
 }
 
 impl actix_web::ResponseError for Error {
+	fn status_code(&self) -> StatusCode {
+		match self {
+			Self::Storage(_) => StatusCode::INTERNAL_SERVER_ERROR,
+			Self::Upstream(_) => StatusCode::INTERNAL_SERVER_ERROR,
+			Self::InvalidDigest => StatusCode::NOT_FOUND,
+			Self::Io(_) => StatusCode::INTERNAL_SERVER_ERROR,
+			Self::Json(_) => StatusCode::INTERNAL_SERVER_ERROR
+		}
+	}
+
+	fn error_response(&self) -> HttpResponse<BoxBody> {
+		let status_code = self.status_code();
+		error!("{}: {}", status_code.as_u16(), self);
+		HttpResponseBuilder::new(status_code)
+			.body(self.to_string())
+	}
 }
 

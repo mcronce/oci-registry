fe95138b8f33e8652db5ed7ddf7ff777c5a962ef
Mike Cronce <mike@quadra-tec.net>
2023-10-14 20:36:39 -0400

    Switch to criterion for benchmarks; divan seems promising, but has significantly larger swings in reported performance than I'm comfortable with, and isn't really providing much better dev experience in return for that

--- benches/main.rs
+++ benches/main.rs
@@ -1,55 +1,74 @@
-use oci_registry::api::split_image;
+use criterion::criterion_group;
+use criterion::criterion_main;
+use criterion::BenchmarkId;
+use criterion::Criterion;
 
-fn main() {
-	divan::main();
+fn split_image_with_ns(c: &mut Criterion) {
+	c.bench_with_input(
+		BenchmarkId::new("split_image", "with ns docker.io/envoyproxy/envoy"),
+		&(Some("docker.io"), "envoyproxy/envoy", ""),
+		|b, &(ns, image, default_ns)| b.iter(|| oci_registry::api::split_image(ns, image, default_ns))
+	);
+
+	c.bench_with_input(
+		BenchmarkId::new("split_image", "with ns docker.io/library/busybox"),
+		&(Some("docker.io"), "library/busybox", ""),
+		|b, &(ns, image, default_ns)| b.iter(|| oci_registry::api::split_image(ns, image, default_ns))
+	);
+
+	c.bench_with_input(
+		BenchmarkId::new("split_image", "with ns gcr.io/distroless/static"),
+		&(Some("gcr.io"), "distroless/static", ""),
+		|b, &(ns, image, default_ns)| b.iter(|| oci_registry::api::split_image(ns, image, default_ns))
+	);
+
+	c.bench_with_input(
+		BenchmarkId::new("split_image", "with ns ghcr.io/buildbarn/bb-runner-installer"),
+		&(Some("ghcr.io"), "buildbarn/bb-runner-installer", ""),
+		|b, &(ns, image, default_ns)| b.iter(|| oci_registry::api::split_image(ns, image, default_ns))
+	);
 }
 
-#[divan::bench]
-fn split_image_with_ns_envoy() {
-	split_image(divan::black_box(Some("docker.io")), divan::black_box("envoyproxy/envoy"), divan::black_box(""));
+fn split_image_without_ns(c: &mut Criterion) {
+	c.bench_with_input(
+		BenchmarkId::new("split_image", "without ns docker.io/envoyproxy/envoy"),
+		&(None, "docker.io/envoyproxy/envoy", ""),
+		|b, &(ns, image, default_ns)| b.iter(|| oci_registry::api::split_image(ns, image, default_ns))
+	);
+
+	c.bench_with_input(
+		BenchmarkId::new("split_image", "without ns docker.io/library/busybox"),
+		&(None, "docker.io/library/busybox", ""),
+		|b, &(ns, image, default_ns)| b.iter(|| oci_registry::api::split_image(ns, image, default_ns))
+	);
+
+	c.bench_with_input(
+		BenchmarkId::new("split_image", "without ns gcr.io/distroless/static"),
+		&(None, "gcr.io/distroless/static", ""),
+		|b, &(ns, image, default_ns)| b.iter(|| oci_registry::api::split_image(ns, image, default_ns))
+	);
+
+	c.bench_with_input(
+		BenchmarkId::new("split_image", "without ns ghcr.io/buildbarn/bb-runner-installer"),
+		&(None, "ghcr.io/buildbarn/bb-runner-installer", ""),
+		|b, &(ns, image, default_ns)| b.iter(|| oci_registry::api::split_image(ns, image, default_ns))
+	);
 }
 
-#[divan::bench]
-fn split_image_with_ns_busybox() {
-	split_image(divan::black_box(Some("docker.io")), divan::black_box("library/busybox"), divan::black_box(""));
+fn split_image_fallback(c: &mut Criterion) {
+	c.bench_with_input(
+		BenchmarkId::new("split_image", "fallback docker.io/envoyproxy/envoy"),
+		&(None, "envoyproxy/envoy", "docker.io"),
+		|b, &(ns, image, default_ns)| b.iter(|| oci_registry::api::split_image(ns, image, default_ns))
+	);
+
+	c.bench_with_input(
+		BenchmarkId::new("split_image", "fallback docker.io/library/busybox"),
+		&(None, "library/busybox", "docker.io"),
+		|b, &(ns, image, default_ns)| b.iter(|| oci_registry::api::split_image(ns, image, default_ns))
+	);
 }
 
-#[divan::bench]
-fn split_image_with_ns_distroless_static() {
-	split_image(divan::black_box(Some("gcr.io")), divan::black_box("distroless/static"), divan::black_box(""));
-}
+criterion_group!(split_image, split_image_with_ns, split_image_without_ns, split_image_fallback);
+criterion_main!(split_image);
 
-#[divan::bench]
-fn split_image_with_ns_buildbarn() {
-	split_image(divan::black_box(Some("ghcr.io")), divan::black_box("buildbarn/bb-runner-installer"), divan::black_box(""));
-}
-
-#[divan::bench]
-fn split_image_without_ns_envoy() {
-	split_image(divan::black_box(None), divan::black_box("docker.io/envoyproxy/envoy"), divan::black_box(""));
-}
-
-#[divan::bench]
-fn split_image_without_ns_busybox() {
-	split_image(divan::black_box(None), divan::black_box("docker.io/library/busybox"), divan::black_box(""));
-}
-
-#[divan::bench]
-fn split_image_without_ns_distroless_static() {
-	split_image(divan::black_box(None), divan::black_box("gcr.io/distroless/static"), divan::black_box(""));
-}
-
-#[divan::bench]
-fn split_image_without_ns_buildbarn() {
-	split_image(divan::black_box(None), divan::black_box("ghcr.io/buildbarn/bb-runner-installer"), divan::black_box(""));
-}
-
-#[divan::bench]
-fn split_image_without_ns_fallback_envoy() {
-	split_image(divan::black_box(None), divan::black_box("envoyproxy/envoy"), divan::black_box("docker.io"));
-}
-
-#[divan::bench]
-fn split_image_without_ns_fallback_busybox() {
-	split_image(divan::black_box(None), divan::black_box("library/busybox"), divan::black_box("docker.io"));
-}

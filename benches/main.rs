331a54e2aab16d75878b37c26ece90a3a85fd094
Mike Cronce <mike@quadra-tec.net>
2023-10-22 12:55:38 -0400

    benches/main.rs:  cargo fmt

--- benches/main.rs
+++ benches/main.rs
@@ -4,23 +4,17 @@ use criterion::BenchmarkId;
 use criterion::Criterion;
 
 fn split_image_with_ns(c: &mut Criterion) {
-	c.bench_with_input(
-		BenchmarkId::new("split_image", "with ns docker.io/envoyproxy/envoy"),
-		&(Some("docker.io"), "envoyproxy/envoy", ""),
-		|b, &(ns, image, default_ns)| b.iter(|| oci_registry::api::split_image(ns, image, default_ns))
-	);
+	c.bench_with_input(BenchmarkId::new("split_image", "with ns docker.io/envoyproxy/envoy"), &(Some("docker.io"), "envoyproxy/envoy", ""), |b, &(ns, image, default_ns)| {
+		b.iter(|| oci_registry::api::split_image(ns, image, default_ns))
+	});
 
-	c.bench_with_input(
-		BenchmarkId::new("split_image", "with ns docker.io/library/busybox"),
-		&(Some("docker.io"), "library/busybox", ""),
-		|b, &(ns, image, default_ns)| b.iter(|| oci_registry::api::split_image(ns, image, default_ns))
-	);
+	c.bench_with_input(BenchmarkId::new("split_image", "with ns docker.io/library/busybox"), &(Some("docker.io"), "library/busybox", ""), |b, &(ns, image, default_ns)| {
+		b.iter(|| oci_registry::api::split_image(ns, image, default_ns))
+	});
 
-	c.bench_with_input(
-		BenchmarkId::new("split_image", "with ns gcr.io/distroless/static"),
-		&(Some("gcr.io"), "distroless/static", ""),
-		|b, &(ns, image, default_ns)| b.iter(|| oci_registry::api::split_image(ns, image, default_ns))
-	);
+	c.bench_with_input(BenchmarkId::new("split_image", "with ns gcr.io/distroless/static"), &(Some("gcr.io"), "distroless/static", ""), |b, &(ns, image, default_ns)| {
+		b.iter(|| oci_registry::api::split_image(ns, image, default_ns))
+	});
 
 	c.bench_with_input(
 		BenchmarkId::new("split_image", "with ns ghcr.io/buildbarn/bb-runner-installer"),
@@ -30,23 +24,17 @@ fn split_image_with_ns(c: &mut Criterion) {
 }
 
 fn split_image_without_ns(c: &mut Criterion) {
-	c.bench_with_input(
-		BenchmarkId::new("split_image", "without ns docker.io/envoyproxy/envoy"),
-		&(None, "docker.io/envoyproxy/envoy", ""),
-		|b, &(ns, image, default_ns)| b.iter(|| oci_registry::api::split_image(ns, image, default_ns))
-	);
+	c.bench_with_input(BenchmarkId::new("split_image", "without ns docker.io/envoyproxy/envoy"), &(None, "docker.io/envoyproxy/envoy", ""), |b, &(ns, image, default_ns)| {
+		b.iter(|| oci_registry::api::split_image(ns, image, default_ns))
+	});
 
-	c.bench_with_input(
-		BenchmarkId::new("split_image", "without ns docker.io/library/busybox"),
-		&(None, "docker.io/library/busybox", ""),
-		|b, &(ns, image, default_ns)| b.iter(|| oci_registry::api::split_image(ns, image, default_ns))
-	);
+	c.bench_with_input(BenchmarkId::new("split_image", "without ns docker.io/library/busybox"), &(None, "docker.io/library/busybox", ""), |b, &(ns, image, default_ns)| {
+		b.iter(|| oci_registry::api::split_image(ns, image, default_ns))
+	});
 
-	c.bench_with_input(
-		BenchmarkId::new("split_image", "without ns gcr.io/distroless/static"),
-		&(None, "gcr.io/distroless/static", ""),
-		|b, &(ns, image, default_ns)| b.iter(|| oci_registry::api::split_image(ns, image, default_ns))
-	);
+	c.bench_with_input(BenchmarkId::new("split_image", "without ns gcr.io/distroless/static"), &(None, "gcr.io/distroless/static", ""), |b, &(ns, image, default_ns)| {
+		b.iter(|| oci_registry::api::split_image(ns, image, default_ns))
+	});
 
 	c.bench_with_input(
 		BenchmarkId::new("split_image", "without ns ghcr.io/buildbarn/bb-runner-installer"),
@@ -56,19 +44,14 @@ fn split_image_without_ns(c: &mut Criterion) {
 }
 
 fn split_image_fallback(c: &mut Criterion) {
-	c.bench_with_input(
-		BenchmarkId::new("split_image", "fallback docker.io/envoyproxy/envoy"),
-		&(None, "envoyproxy/envoy", "docker.io"),
-		|b, &(ns, image, default_ns)| b.iter(|| oci_registry::api::split_image(ns, image, default_ns))
-	);
+	c.bench_with_input(BenchmarkId::new("split_image", "fallback docker.io/envoyproxy/envoy"), &(None, "envoyproxy/envoy", "docker.io"), |b, &(ns, image, default_ns)| {
+		b.iter(|| oci_registry::api::split_image(ns, image, default_ns))
+	});
 
-	c.bench_with_input(
-		BenchmarkId::new("split_image", "fallback docker.io/library/busybox"),
-		&(None, "library/busybox", "docker.io"),
-		|b, &(ns, image, default_ns)| b.iter(|| oci_registry::api::split_image(ns, image, default_ns))
-	);
+	c.bench_with_input(BenchmarkId::new("split_image", "fallback docker.io/library/busybox"), &(None, "library/busybox", "docker.io"), |b, &(ns, image, default_ns)| {
+		b.iter(|| oci_registry::api::split_image(ns, image, default_ns))
+	});
 }
 
 criterion_group!(split_image, split_image_with_ns, split_image_without_ns, split_image_fallback);
 criterion_main!(split_image);
-

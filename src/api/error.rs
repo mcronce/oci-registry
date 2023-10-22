8ee245956886e5de614df853db1d314d532b98d7
Mike Cronce <mike@quadra-tec.net>
2023-10-22 13:35:58 -0400

    src/api/error.rs:  Support 404s from storage errors

--- src/api/error.rs
+++ src/api/error.rs
@@ -3,12 +3,17 @@ use actix_web::http::StatusCode;
 use actix_web::HttpResponse;
 use actix_web::HttpResponseBuilder;
 use dkregistry::errors::Error as Upstream;
+use rusoto_core::request::BufferedHttpResponse;
+use rusoto_core::RusotoError;
+use rusoto_s3::GetObjectError;
 use tracing::error;
+
+use crate::storage::Error as Storage;
 
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
 	#[error("Error with storage subsystem: {0}")]
-	Storage(#[from] crate::storage::Error),
+	Storage(#[from] Storage),
 	#[error("Error with upstream registry: {0}")]
 	Upstream(#[from] Upstream),
 	#[error("Not found")]
@@ -24,7 +29,12 @@ pub enum Error {
 impl actix_web::ResponseError for Error {
 	fn status_code(&self) -> StatusCode {
 		match self {
-			Self::Storage(_) => StatusCode::INTERNAL_SERVER_ERROR,
+			Self::Storage(e) => match e {
+				Storage::Io(e) if e.kind() == std::io::ErrorKind::NotFound => StatusCode::NOT_FOUND,
+				Storage::RusotoGet(RusotoError::Service(GetObjectError::NoSuchKey(_))) => StatusCode::NOT_FOUND,
+				Storage::RusotoDelete(RusotoError::Unknown(BufferedHttpResponse { status, .. })) if *status == StatusCode::NOT_FOUND => StatusCode::NOT_FOUND,
+				_ => StatusCode::INTERNAL_SERVER_ERROR
+			},
 			Self::Upstream(e) => match e {
 				Upstream::UnexpectedHttpStatus(StatusCode::NOT_FOUND) => StatusCode::NOT_FOUND,
 				Upstream::UnexpectedHttpStatus(_) => StatusCode::INTERNAL_SERVER_ERROR,
